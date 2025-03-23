b6e157f1a9a560570e1a0fd717e72a440d73ea98
Nightyyi <163418554+Nightyyi@users.noreply.github.com>
2025-03-23 15:27:28 +0800

    finishd converter.py for now

--- converter.py
+++ converter.py
@@ -1,14 +1,29 @@
-import subprocess
+import subprocess, shutil
 from pathlib import Path 
 
-p = Path('.')
-list_of_sprites = list(p.glob('**/*.aseprite'))
+omit_list = ["assets\\cutscene\\first_cutscene.aseprite"]
+
+
+p = Path('.\\assets\\')
+list_of_sprites = list(p.glob('**\\*.aseprite'))
+
+temp_list = []
+for sprite in list_of_sprites:
+    if not (str(sprite) in omit_list):
+        temp_list.append(temp_list)
+        
+list_of_sprites = temp_list
+
+if len(list_of_sprites) == 0:
+    print("No .aseprite files to convert to .png")
+    quit()
+
 for sprite in list_of_sprites:
     print(str(sprite))
 
 run = True
 while run:
-    confirmation = input("Are you sure you want to convert ALL of these .aseprite files to .png/n (Y) or (N)\n")
+    confirmation = input("Are you sure you want to convert ALL of these .aseprite files to .png\n (Y) or (N)\n")
     confirmation = confirmation.upper()
     if confirmation == 'N':
         quit()
@@ -17,13 +32,16 @@ while run:
     else:
         print("Invalid answer.")
 
-omit_list = ["assets\\cutscene\\first_cutscene.aseprite"]
+current_directory = Path.cwd()
+
 
 for sprite in list_of_sprites:
     sliced_sprite = str(sprite).split(".")
-    if not (str(sprite) in omit_list):
 
-        subprocess.call(["aseprite", "-b", str(sprite), "--save-as", sliced_sprite[0]+".png"])
-        print(str(sprite),"done")
-    else:
-        print(str(sprite), "has been omitted")
+    subprocess.call(["aseprite", "-b", str(sprite), "--save-as", sliced_sprite[0]+".png"])
+    print(str(sprite),"done being converted")
+    
+    source = str(current_directory / sprite)
+    destination = str(current_directory)+"\\aseprites\\"
+    shutil.move(source, destination)
+    print(source, "->", destination)
